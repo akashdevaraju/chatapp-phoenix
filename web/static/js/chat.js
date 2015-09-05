50a1f06549b10b65db8dd81b575ec13a02cc2cd7
Akash Devaraju <akash.devaraju@icicletech.com>
2015-09-05 13:59:08 +0530

    [Chat] Move chat logic from socket.js to custom chat.js

---
+++ web/static/js/chat.js
@@ -0,0 +1,5 @@
+// Now that you are connected, you can join channels with a topic:
+let channel = socket.channel("topic:subtopic", {})
+channel.join()
+  .receive("ok", resp => { console.log("Joined successfully", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) })
