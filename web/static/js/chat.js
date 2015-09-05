281da343acd6757d9fff6460b06cc90895e2639e
Akash Devaraju <akash.devaraju@icicletech.com>
2015-09-05 14:00:30 +0530

    [Rooms] Accept all connects

--- web/static/js/chat.js
+++ web/static/js/chat.js
@@ -1,7 +1,7 @@
 import socket from './socket'
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
+let channel = socket.channel("rooms:*", {})
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
