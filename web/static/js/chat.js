e8765d4796d8cb84c79595810734ca7b1ba11081
Akash Devaraju <akash.devaraju@icicletech.com>
2015-09-05 14:27:20 +0530

    [Socket] Added js for handling messages

--- web/static/js/chat.js
+++ web/static/js/chat.js
@@ -5,3 +5,13 @@ let channel = socket.channel("rooms:*", {})
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
+
+// Take message provided in inputbox
+$('#message-message').off("keypress").on("keypress", function(event) {
+  if(event.keyCode == 13) {
+    channel.push("message:new", {
+      message: $('#message-message').val()
+    })
+    $('#message-message').val("")
+  }
+});
